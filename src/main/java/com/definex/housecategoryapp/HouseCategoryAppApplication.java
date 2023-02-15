d42eac19f6fc56f92153555eb70651e98ee40bc8
bunyaminyerles <bnymnyrls@hotmail.com>
2023-02-15 17:21:21 +0300

    It initializes and creates springBootApplication.

---
+++ src/main/java/com/definex/housecategoryapp/HouseCategoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.definex.housecategoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseCategoryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseCategoryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/definex/housecategoryapp/HouseCategoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.definex.housecategoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseCategoryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
